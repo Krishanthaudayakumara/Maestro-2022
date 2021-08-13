bc4430cf375b07f36460ef1f3265321dd105deb7
Ranul Navojith <ranulnavoijith@gmail.com>
2021-08-13 16:01:00 +0530

    deployed to firebase

--- forms/contact.php
+++ forms/contact.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
@@ -7,21 +7,21 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // $receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,9 +33,37 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+//   $contact->add_message( $_POST['name'], 'From');
+//   $contact->add_message( $_POST['email'], 'Email');
+//   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
-?>
+//   echo $contact->send();
+// ?>
+
+<?php 
+	
+	// checking if the form is submit
+	if ( isset($_POST['submit']) ) {
+		$fullname	= $_POST['name'];
+		$email		= $_POST['email'];
+		$subject	= $_POST['text'];
+		$message	= $_POST['message'];
+
+		$to	 		  = 'ranul@rotaractmora.org';
+		$mail_subject = 'Message from Website';
+		$email_body   = "Message from Contact Us Page of the Website: <br>";
+		$email_body   .= "<b>From:</b> {$fullname} <br>";
+		$email_body   .= "<b>Subject:</b> {$subject} <br>";
+		$email_body   .= "<b>Message:</b><br>" . nl2br(strip_tags($message));
+
+		$header       = "From: {$email}\r\nContent-Type: text/html;";
+
+		$send_mail_result = mail($to, $mail_subject, $email_body, $header);
+
+		if ( $send_mail_result ) {
+			echo "Message Sent.";
+		} else {
+			echo "Message Not Sent.";
+		}
+	}
+ ?>
